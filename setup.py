493f067dc038a08a5cb6f64cbcdec672e7841379
Aleksandr <alexander.ivanov.35@gmail.com>
2020-11-16 13:50:43 +0900

    lib fix

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='validatorlib',
+    packages=find_packages(),
+    version='0.1',
+    description='A validator library',
+    author='alexander.ivanov.35@gmail.com',
+    license='MIT',
+	install_requires=[],
+    setup_requires=[],
+    tests_require=[],
+    test_suite='tests',
+)
